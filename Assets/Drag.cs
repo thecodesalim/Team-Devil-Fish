3b5751c9e16f104345ad95d9a3dffc8f1c0a3bf1
Abubakar Salim <salimabubakar@Abubakars-MacBook-Pro.local>
2019-06-18 14:17:40 +0100

    Grid selection works now

---
+++ Assets/Drag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// © 2017 TheFlyingKeyboard and released under MIT License
+// theflyingkeyboard.net
+public class Drag : MonoBehaviour {
+    public GameObject obj;
+    private GameObject wall;
+    public float moveSpeed;
+    public float offset = 0.05f;
+    private bool following;
+    // Use this for initialization
+    void Start () {
+        following = false;
+        offset += 10;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown (0) && ((Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position).magnitude <= offset)) {
+            
+            GameObject wall = Instantiate (obj, transform.position, Quaternion.identity) as GameObject;
+            if (following) {
+                following = false;
+            } else {
+                following = true;
+            }
+        }
+        if (following) {
+            
+            wall.transform.position = Vector2.Lerp (transform.position, Camera.main.ScreenToWorldPoint (Input.mousePosition), moveSpeed);
+        }
+    }
+}
