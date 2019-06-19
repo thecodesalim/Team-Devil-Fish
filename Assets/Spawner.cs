e9c753bc00d7864ca7cf7f21c5f2ea76aa1624e0
Abubakar Salim <salimabubakar@pc-143-111.customer.crown.crm.lan>
2019-06-19 08:46:35 +0100

    dropping wall works

---
+++ Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer--;
+        if( timer < 0) {
+            Instantiate(enemy,transform.position, Quaternion.identity);
+            timer = 50;
+        }
+    }
+}
